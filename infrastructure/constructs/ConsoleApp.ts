9f2c5c2c6de66f67625c9f340222108c5176b8d9
Jay RIvers <113142623+jrivers-iclass@users.noreply.github.com>
2024-05-26 13:12:53 -0500

    LambdaRole construct and ConsoleApp construct

--- infrastructure/constructs/ConsoleApp.ts
+++ infrastructure/constructs/ConsoleApp.ts
@@ -1,5 +1,4 @@
 import * as pulumi from "@pulumi/pulumi";
-import * as aws from "@pulumi/aws";
 import {PhpFpmFunction} from "../function/PhpFpmFunction";
 import {PhpConsoleFunction} from "../function/PhpConsoleFunction";
 import {LambdaRole} from "./LambdaRole";
