bb50213d3ef1088fadaaa767e188bce9483a6a55
Jay RIvers <113142623+jrivers-iclass@users.noreply.github.com>
2024-05-26 18:47:33 -0500

    Beginning work on database support

--- infrastructure/index.ts
+++ infrastructure/index.ts
@@ -2,6 +2,72 @@ import * as pulumi from "@pulumi/pulumi";
 import * as aws from "@pulumi/aws";
 import {WebApp, ConsoleApp, LambdaRole, S3BucketPolicy} from "./constructs";
 import {SqsWorker} from "./constructs/SqsWorker";
+import * as awsx from "@pulumi/awsx";
+
+const vpc = new awsx.ec2.Vpc("lambda-test-vpc", {
+    cidrBlock: "10.10.0.0/16",
+    numberOfAvailabilityZones: 2,
+    subnetStrategy: "Auto",
+    subnetSpecs: [
+        { type: awsx.ec2.SubnetType.Public, cidrMask: 24 },
+        { type: awsx.ec2.SubnetType.Private, cidrMask: 24 },
+    ],
+    natGateways: {
+        strategy: awsx.ec2.NatGatewayStrategy.Single,
+    },
+});
+
+const securityGroup = new aws.ec2.SecurityGroup("auroraSecurityGroup", {
+    vpcId: vpc.vpcId,
+    description: "Allow access to Aurora",
+    ingress: [
+        {
+            protocol: "tcp",
+            fromPort: 3306,
+            toPort: 3306,
+            cidrBlocks: ["10.0.0.0/16"],
+        },
+    ],
+    egress: [
+        {
+            protocol: "-1",
+            fromPort: 0,
+            toPort: 0,
+            cidrBlocks: ["0.0.0.0/0"],
+        },
+    ],
+    tags: {
+        Name: "auroraSecurityGroup",
+    },
+});
+
+// Create an Aurora Serverless v2 cluster
+const auroraCluster = new aws.rds.Cluster("laravel-test-cluster", {
+    engine: aws.rds.EngineType.AuroraMysql,
+    engineVersion: "8.0.mysql_aurora.3.06.0",
+    engineMode: aws.rds.EngineMode.Provisioned,
+    databaseName: "laravelTest",
+    masterUsername: "admin",
+    manageMasterUserPassword: true,
+    serverlessv2ScalingConfiguration: {
+        maxCapacity: 1,
+        minCapacity: 0.5,
+    },
+    skipFinalSnapshot: true,
+    vpcSecurityGroupIds: [securityGroup.id],
+    dbSubnetGroupName: new aws.rds.SubnetGroup("auroraSubnetGroup", {
+        subnetIds: vpc.privateSubnetIds,
+        name: "aurora-subnet-group",
+    }).name,
+});
+
+new aws.rds.ClusterInstance("laravel-test-instance", {
+    clusterIdentifier: auroraCluster.id,
+    instanceClass: "db.serverless",
+    engine: auroraCluster.engine.apply(engine => engine as aws.rds.EngineType),
+    dbSubnetGroupName: auroraCluster.dbSubnetGroupName,
+    publiclyAccessible: false,
+});
 
 // Create an AWS resource (S3 Bucket)
 const bucket = new aws.s3.Bucket("bref-example-bucket");
@@ -10,10 +76,21 @@ const bucket = new aws.s3.Bucket("bref-example-bucket");
 const lambdaRole = new LambdaRole("lambdaRole");
 lambdaRole.addPolicy("s3", new S3BucketPolicy(bucket).bucketPolicy);
 
+// const auroraSecret = aws.secretsmanager.getSecret({
+//     arn: auroraCluster.masterUserSecrets.apply(
+//         secrets => secrets[0].secretArn
+//     ),
+// });
+
 const environment = {
     FILESYSTEM_DISK: "s3",
     AWS_BUCKET: bucket.bucket,
     DB_DATABASE: ":memory:",
+    DB_CONNECTION: "mysql",
+    DB_HOST: auroraCluster.endpoint,
+    DB_PORT: "3306",
+    DB_USERNAME: "admin",
+    DB_PASSWORD: "TODO: REPLACE ME",
 };
 
 // Create the WebApp
@@ -22,21 +99,26 @@ const webApp = new WebApp(
     new pulumi.asset.FileArchive("../laravel"),
     lambdaRole,
     {BREF_LOOP_MAX: 250, ...environment},
-    true
+    true,
 );
 
 const consoleApp = new ConsoleApp(
     "laravel-test-artisan",
     new pulumi.asset.FileArchive("../laravel"),
     lambdaRole,
-    environment
+    environment,
 );
 
 const sqsWorker = new SqsWorker(
     "laravel-test-worker",
     new pulumi.asset.FileArchive("../laravel"),
     lambdaRole,
-    environment
+    environment,
+    undefined,
+    undefined,
+    undefined,
+    undefined,
+    undefined
 );
 
 
@@ -56,3 +138,7 @@ export const workerLambdaName = sqsWorker.phpFunction.lambda.name;
 export const queueUrl = sqsWorker.queue.url;
 // Export octane flag
 export const useOctane = webApp.useOctane;
+export const vpcId = vpc.vpcId;
+export const auroraClusterId = auroraCluster.id;
+export const auroraClusterEndpoint = auroraCluster.endpoint;
+export const auroraClusterSecretArn = auroraCluster.masterUserSecrets[0].secretArn;
