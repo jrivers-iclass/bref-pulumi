83f0c2137f81c79163ab163c1ed64886b6f9e606
J. Fulbright <jeremiah@iclasspro.com>
2024-09-24 11:22:16 -0500

    add ingress/egress rules for pulumi best practice

--- infrastructure/vpc/AuroraSecurityGroup.ts
+++ infrastructure/vpc/AuroraSecurityGroup.ts
@@ -1,30 +1,30 @@
-import * as aws from "@pulumi/aws";
-import {SimpleVpc} from "./SimpleVpc";
-
+import * as aws from '@pulumi/aws';
+import { SimpleVpc } from './SimpleVpc';
 
 export class AuroraSecurityGroup {
     public readonly securityGroup: aws.ec2.SecurityGroup;
-    constructor(name: string, vpc: SimpleVpc)
-    {
+    constructor(name: string, vpc: SimpleVpc) {
         this.securityGroup = new aws.ec2.SecurityGroup(name, {
             vpcId: vpc.vpc.vpcId,
-            description: "Allow access to Aurora",
-            ingress: [
-                {
-                    protocol: "tcp",
-                    fromPort: 3306,
-                    toPort: 3306,
-                    cidrBlocks: ["10.0.0.0/16"],
-                },
-            ],
-            egress: [
-                {
-                    protocol: "-1",
-                    fromPort: 0,
-                    toPort: 0,
-                    cidrBlocks: ["0.0.0.0/0"],
-                },
-            ]
+            description: 'Allow access to Aurora',
+        });
+
+        // Ingress rule
+        const allowAuroraAccess = new aws.vpc.SecurityGroupIngressRule('allow-aurora-access', {
+            securityGroupId: this.securityGroup.id,
+            ipProtocol: 'tcp',
+            cidrBlocks: ['10.0.0.0/16'],
+            fromPort: 3306,
+            toPort: 3306,
+        });
+
+        // Egress rule
+        const allowAllOutbound = new aws.vpc.SecurityGroupEgressRule('allow-all-outbound', {
+            securityGroupId: this.securityGroup.id,
+            ipProtocol: '-1',
+            cidrBlocks: ['0.0.0.0/0'],
+            fromPort: 0,
+            toPort: 0,
         });
     }
 }
